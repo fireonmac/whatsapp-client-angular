da43abf4e78219b32053a25accb2b6d2cedbe643
Paul Noh <fireonmac@gmail.com>
2024-03-02 06:07:09 +0900

    style: revert daisyui theme color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,17 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: [
+  //     {
+  //       light: {
+  //         ...require("daisyui/src/theming/themes")["light"],
+  //         primary: "#4BA586",
+  //         ".btn-primary": {
+  //           color: "White",
+  //         },
+  //       },
+  //     },
+  //   ],
+  // },
 };
